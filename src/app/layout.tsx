78685402fc33792f69e43ee22275839eb9e01683
Thomas Mcgrath <tomrossmcgrath@gmail.com>
2025-05-01 14:54:04 +1000

    Styled navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,14 @@
 import { Inter } from "next/font/google";
+// import { IBM_Plex_Sans } from "next/font/google";
+
 import Navbar from "@/components/navbar/Navbar";
+import "@/styles/globals.css";
+
+// const IBM = IBM_Plex_Sans({
+//   weight: "400",
+//   subsets: ["latin"],
+//   display: "swap",
+// });
 
 const inter = Inter({
   subsets: ["latin"],
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,21 @@
 import styles from "./Navbar.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav className={styles.nav}>
       <span>
-        <Link href="/">Logo</Link>
+        <Link href="/">
+          <Image src="/logo-small.png" alt="Logo" width="50" height="50" />
+        </Link>
       </span>
       <span className={styles.container}>
         <Link href="">Help</Link>
         <Link href="">Bookings</Link>
-        <Link href="">Login</Link>
+        <Link href="">
+          <button className={styles.loginBtn}>Login</button>
+        </Link>
       </span>
     </nav>
   );
