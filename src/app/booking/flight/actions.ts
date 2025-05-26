cd4397a09e9704775e54cf8ad3914f951deb7f1d
Thomas Mcgrath <tomrossmcgrath@gmail.com>
2025-05-26 15:53:45 +1000

    this wont work locally

--- src/app/booking/flight/actions.ts
+++ src/app/booking/flight/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import flightReviver from "@/utils/flightReviver";
-const URL = "http://" + process.env.NEXT_PUBLIC_VERCEL_URL + "/api/booking/flight?";
+const URL = "http://" + process.env.VERCEL_URL + "/api/booking/flight?";
 
 export const fetcher = async (searchParams: URLSearchParams) => {
   const response = await fetch(URL + searchParams);
